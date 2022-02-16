114959d2fadaf789b5832537336acdac7be909b4
Tarje Wang <tarjewang@dhcp-10-22-57-26.wlan.ntnu.no>
2022-02-16 12:02:15 +0100

    added some important files, not fully converted yet

---
+++ CNN.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+import numpy as np
+
+
+from keras.layers import (
+    Dense,
+    Dropout,
+    Reshape,
+    Flatten,
+    InputLayer,
+    Conv1D,
+    GlobalMaxPooling1D,
+)
+from keras.losses import KLDivergence
+from keras.models import Sequential, load_model
+from keras.models import Model
+from keras import metrics
+import tensorflow.keras.optimizers as optimizers
+import tensorflow as tf
+from tensorflow.python.keras.backend import (
+    binary_crossentropy,
+    conv1d,
+    categorical_crossentropy,
+)
+from tensorflow.python.keras.layers.core import Activation
+from tensorflow.python.keras.layers.pooling import MaxPool1D
+from tensorflow.python.keras.losses import BinaryCrossentropy
+
+from tcn import TCN
+
+
+from data_processing import input_length, training_length
+
+
+from data_processing import training_data_x, training_data_y, test_data_x, test_data_y
+
+# "learning_rate": 0.001,  # Learning rate the neural net
+# activation functions: linear, sigmoid, tanh, or relu, need to be at least same lenght as hidden layers.
+# "anet_optimizer": "Adam",  # Adam, SGD, Adagrad, RMSprop
+
+
+class CNN:
+    def __init__(self) -> None:
+        return None
+
+    def initialize(self, parameters):
+        if parameters["starting_cnn"] != "":
+            self.load_model(parameters["starting_cnn"])
+            return self
+
+        self.model = Sequential()
+
+        self.model.add(
+            InputLayer(input_shape=(training_length + 1, input_length))
+        )  # need to add input shape here
+
+        # adding the TCN layer here
+        self.model.add(
+            TCN(
+                nb_filters=parameters["TCN_nb_filters"],
+                kernel_size=parameters["TCN_kernel_size"],
+                nb_stacks=parameters["TCN_nb_stacks"],
+                dilations=parameters["TCN_dilations"],
+                padding=parameters["TCN_padding"],
+                dropout_rate=parameters["TCN_dropout_rate"],
+                activation=parameters["TCN_activiation"],
+            )
+        )
+
+        # adding dense layers
+        for index, layer in enumerate(parameters["hidden_layers"]):
+            self.model.add(
+                Dense(layer, activation=parameters["activation_functions"][index])
+            )
+
+        # Will need to map to a single number, last layer
+        self.model.add(Dense(1, activation=parameters["last_layer_activation"]))
+
+        selected_optimizer = getattr(optimizers, parameters["optimizer"])(
+            learning_rate=parameters["learning_rate"]
+        )
+
+        self.model.compile(
+            loss=parameters["loss"],
+            optimizer=selected_optimizer,
+            metrics=parameters["metrics"],
+        )
+
+        self.model.summary()
+
+        return self
+
+    def predict(self, input):
+        return self.model.predict(input)
+
+    def fit(
+        self, features, targets, batch_size, epochs, validation_split, shuffle, verbose
+    ):
+
+        ret = self.model.fit(
+            x=features,
+            y=targets,
+            batch_size=batch_size,
+            epochs=epochs,
+            validation_split=validation_split,
+            shuffle=shuffle,
+            verbose=verbose,
+        )
+
+        # loss = ret.history.get("loss", [None])
+        # mae = ret.history.get("MAE", [None])
+
+        return ret
+
+    def save_model(self, path):
+        self.model.save(path)
+
+    def load_model(self, path):
+        self.model = load_model(path)
+
+
+# history = self.model.fit(x=features, y=targets, batch_size = how many to fit at a time, epochs = 5, validation_split = 0.1,shuffle = True) # how to train
+# self.model( [input] ) -> output
---
+++ error_metrics.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+# Will contain the following error metrics: RMSE, MAPE and MAE
+# Input for all will be a numpy array containing tuples with [forecast, target]
+
+
+def MAE_error(data):  # good
+
+    return np.average(list(np.abs(x[0] - x[1]) for x in data))
+
+
+def MAPE_error(data):  # good
+
+    output_sum = 0
+    for x in data:
+        devi = np.abs(x[1] - x[0]) / x[1]
+        output_sum += devi
+
+    return 100 * (output_sum / len(data))
+
+
+def RMSE_error(data):
+
+    output_sum = 0
+    for x in data:
+        output_sum += (x[0] - x[1]) ** 2
+    output_sum = output_sum / len(data)
+    return output_sum ** 0.5
+
+
+def SMAPE_error(data):
+    output_sum = 0
+    for x in data:
+        top = np.abs(x[1] - x[0])
+        bottom = (np.abs(x[1]) + np.abs(x[0])) / 2
+        output_sum += top / bottom
+    return 100 * (output_sum / len(data))
+
+
+def get_metrics(test_list):
+    metrics = {
+        "MAE": MAE_error(test_list),
+        "MAPE": MAPE_error(test_list),
+        "RMSE": RMSE_error(test_list),
+        "SMAPE": SMAPE_error(test_list),
+    }
+    return metrics
