3f7853724ea7e16ab9570d87ab0e8d754d018238
Tarje Wang <tarjewang@dhcp-10-22-56-240.wlan.ntnu.no>
2022-04-21 12:20:50 +0200

    as

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,25 @@
+from cProfile import label
+from cgi import test
 import csv
+from re import A
+import wave
+from bleach import clean
 from numpy.lib.function_base import average
 import pandas as pd
 import datetime
 import numpy as np
+from pyparsing import col
 import seaborn as sn
 import matplotlib.pyplot as plt
+
+#Imports for preprocessing
+#from data_erling.data_transforms.min_max_transform import Min_max_scaler
+#from data_erling.data_transforms.standardize import Standardizer
+from data_erling.data_transforms.column_types import time_series_columns
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, normalize
+from sklearn.decomposition import PCA
+import pywt
+from statsmodels.robust import mad
 
 """
 The test case study is designed by using data from 1 Jan 2014 to 31 Dec 2019 as training data and 1 Jan 2020 to
@@ -90,8 +105,18 @@ Marius & Jake Estimate:
 
 # these are the vars which can be changed
 selected_colums = [
-    "EEX",
-  "System Price"
+    "SE1",
+    #"SE2",
+    #"SE3",
+    #"SE4"
+    #"DK1",
+    #"DK2",
+    #"FI",
+    #"Oslo",
+    "Kr.sand",
+    "Tromsø"
+    #"Bergen",
+    #"Molde"
 ] 
 
 output_variable = ( 
@@ -102,6 +127,12 @@ if output_variable not in selected_colums:
 if "Date" in selected_colums:
     print("REMOVE DATE FROM SELECTED COLUMS/ OUTPUT VARIABLE, NOT FLOAT!")
 
+#Data processing - normalization and standardization - Standard is standardization
+standardize_data = True
+min_max_normalize_data = False
+
+if standardize_data and min_max_normalize_data:
+    min_max_normalize_data = False
 
 # Example format: date = 2014-01-01, hour = 3 (03:00-04:00)
 
@@ -166,7 +197,96 @@ training_test_split = date_hour_list.index('2020-01-01-0')
 training_data = data_used[0:training_test_split] # 2014-01-01-0 - 2019-12-31-23 # 6 years
 test_data = data_used[training_test_split:] # 2020-01-01-0 - 2020-12-31-23 # 1 year , need to talk about COVID
 
+
+def find_pca_explained_variance(pca_obj):
+    plt.plot(np.cumsum(pca_obj.explained_variance_ratio_))
+    plt.show()
+
+
+def principal_component_analysis(data, explained_variance=0.99):
+    pca = PCA(n_components=explained_variance)
+    pca = pca.fit(data)
+    #find_pca_explained_variance(pca)
+    return pca.transform(data), pca
+
+    
+
+training_data_trans, pca_obj = principal_component_analysis(training_data)
+
 # NEED TO DO SOME DATA PREPROCESSING HERE !!! 
+
+active_price_colums = [x for x in time_series_columns if x in training_data.columns]
+
+
+def standardize_data_func(training_data, test_data, active_cols):
+    pd.set_option("mode.chained_assignment", None) 
+    training_length = len(training_data)
+    test_length = len(test_data)
+    for col in active_cols:
+        scaler = StandardScaler()
+        scaler.fit(training_data[col].to_numpy().reshape((-1, 1)))
+        training_data[col] = scaler.transform(training_data[col].to_numpy().reshape((-1, 1))).reshape((training_length,))
+        test_data[col] = scaler.transform(test_data[col].to_numpy().reshape((-1, 1))).reshape((test_length,))
+    return pd.DataFrame(training_data, columns=training_data.columns), pd.DataFrame(test_data, columns=test_data.columns)
+
+
+def min_max_normalize_data_func(training_data, test_data, active_cols):
+    pd.set_option("mode.chained_assignment", None)
+    training_length = len(training_data)
+    test_length = len(test_data)
+    for col in active_cols:
+        scaler = MinMaxScaler()
+        scaler.fit(training_data[col].to_numpy().reshape((-1, 1)))
+        training_data[col] = scaler.transform(training_data[col].to_numpy().reshape((-1, 1))).reshape((training_length,))
+        test_data[col] = scaler.transform(test_data[col].to_numpy().reshape((-1, 1))).reshape((test_length,))
+    return pd.DataFrame(training_data, columns=training_data.columns), pd.DataFrame(test_data, columns=test_data.columns)
+
+if standardize_data:
+    training_data, test_data = standardize_data_func(training_data, test_data, active_price_colums)
+elif min_max_normalize_data:
+    training_data, test_data = min_max_normalize_data_func(training_data, test_data, active_price_colums)
+else:
+    print('No standardization/normalization was made to the data')
+
+
+def unit_vector_normalization(data):
+    return pd.DataFrame(normalize(data), columns=data.columns)
+
+
+
+#Can only be used for training data - not sure if it is scientifically valid to use it for test data
+def wavelet_transform_data(data, mother_wavelet='coif1', n_levels=30, plot_transform=False):
+    data_numpy = data.to_numpy()
+    cols = data.columns
+    steps = len(data)
+    fig, axs = plt.subplots(len(cols))
+    for i in range(len(cols)):
+        time_series = data[cols[i]].to_numpy().reshape((steps,))
+        coeffs = pywt.wavedec(time_series, mother_wavelet, level=n_levels)
+        sigma = mad(coeffs[-1])/0.5
+        thresh = sigma * np.sqrt(2 * np.log(steps))
+        coeffs[1:] = (pywt.threshold(i, value=thresh, mode='hard') for i in coeffs[1:])
+        clean = pywt.waverec(coeffs[1:], mother_wavelet)
+        if plot_transform:
+            axs[i].plot(data_numpy[:, i], c='b')
+            axs[i].plot(clean, c='r')
+            axs[i].set_title(cols[i])
+        data_numpy[:, i] = clean
+    if plot_transform:
+        plt.show()
+    return pd.DataFrame(data_numpy, columns=cols)
+
+
+
+def asinh_transform_dataset(training_data, test_data):
+    cols = training_data.columns
+    training_data = training_data.to_numpy()
+    test_data = test_data.to_numpy()
+    for row in range(len(training_data)):
+        training_data[row, :] = np.arcsinh(training_data[row, :])   #Numpy is a lot faster here than tensorflow
+    for row in range(len(test_data)):
+        test_data[row, :] = np.arcsinh(test_data[row, :])
+    return pd.DataFrame(training_data, columns=cols), pd.DataFrame(test_data, columns=cols)
 
 
 # split into x (input vars) and y (target system price) data
